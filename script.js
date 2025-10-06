b48854bdb1d1b578d41fd38bf69d80428280aba9
TurboGarry <realgaurav68@gmail.com>
2025-10-06 23:10:47 +0530

    add conditions for weather

--- script.js
+++ script.js
@@ -41,6 +41,16 @@ async function getFetchData(endPoint, city){
     return response.json(); 
 }
 
+function getWeatherIcon(id){
+    if (id <= 232) return 'thunderstorm.svg';
+    if (id <= 321) return 'drizzle.svg';
+    if (id <= 531) return 'rain.svg';
+    if (id <= 622) return 'snow.svg';
+    if (id <= 781) return 'atmosphere.svg';
+    if (id <= 800) return 'clear.svg';
+    else return 'clouds.svg';
+}
+
 async function updateWeatherInfo(city){
     const weatherData = await getFetchData('weather', city);
 
